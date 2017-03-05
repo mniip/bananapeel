8eb78748d6bb115db68ebcc4de61d4f34bfc9c24
mniip <mniip@mniip.com>
2017-03-05 16:25:20 +0300

    Implement some basic server hooks

--- application/src/main/java/com/mniip/bananapeel/IRCCommandHandler.java
+++ application/src/main/java/com/mniip/bananapeel/IRCCommandHandler.java
@@ -25,10 +25,57 @@ public class IRCCommandHandler
 			handleUnhandled(srv, msg);
 		}
 		catch(IllegalAccessException e) { }
-		catch(InvocationTargetException e) { }
+		catch(InvocationTargetException e)
+		{
+			if(e.getCause() instanceof RuntimeException)
+				throw (RuntimeException)e.getCause();
+		}
+	}
+
+	@IRCCommandHandler.Hook
+	private static void handleJOIN(Server srv, IRCMessage msg)
+	{
+		if(msg.args.length >= 1 && msg.source != null)
+		{
+			String nick = msg.getNick();
+			String channel = msg.args[0];
+			Tab tab;
+			if(nick.equals(srv.ourNick))
+			{
+				tab = srv.getService().createTab();
+				tab.setServer(srv);
+				tab.setTitle(channel);
+			}
+			else
+				tab = srv.getService().findTab(srv, channel);
+			if(tab != null)
+				tab.putLine("* " + nick + " joined " + channel);
+
+		}
+	}
+
+	@IRCCommandHandler.Hook
+	private static void handlePRIVMSG(Server srv, IRCMessage msg)
+	{
+		if(msg.args.length >= 2)
+		{
+			String nick = msg.getNick();
+			String target = msg.args[0];
+			String text = msg.args[1];
+			String location = target.equals(srv.ourNick) ? nick : target;
+			Tab tab = srv.getService().findTab(srv, location);
+			if(tab == null)
+			{
+				tab = srv.getService().createTab();
+				tab.setServer(srv);
+				tab.setTitle(nick); /* TODO */
+			}
+			tab.putLine("<" + nick + "> " + text);
+		}
 	}
 
 	private static void handleUnhandled(Server srv, IRCMessage msg)
 	{
+		srv.getTab().putLine(msg.toIRC());
 	}
 }
